e07902d586a824fc25b8f6409b491abe9bd1371a
daniel berrocal <jdanielb1992@gmail.com>
2013-04-18 20:32:01 -0600

    clases AST

---
+++ Compilador/src/AST/ASTCuerpoPrograma.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AST;
+
+/**
+ *
+ * @author jdbr
+ */
+public class ASTCuerpoPrograma extends ASTProgram{
+    public ASTCuerpoPrograma (){
+        super();
+        
+    }
+    
+}
---
+++ Compilador/src/AST/ASTProgram.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AST;
+
+/**
+ *
+ * @author jdbr
+ */
+public abstract class ASTProgram extends AST{
+    public ASTProgram (){
+        super();
+        
+    }
+    
+    
+}
