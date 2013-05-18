23a0fa51a69af4168a9ec0c06be9cecfe4656612
daniel berrocal <jdanielb1992@gmail.com>
2013-05-18 13:52:19 -0600

    Compilador 2.1.1

--- compilador2/COMPILER/src/compiler/COMPILER.java
+++ compilador2/COMPILER/src/compiler/COMPILER.java
@@ -2,18 +2,20 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Compi;
+package compiler;
+
 import Compilador.*;
+
 /**
  *
  * @author jdbr
  */
-public class Compilador {
-     /**
+public class COMPILER {
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
        new Ventana().show();
-    
-}
+    }
 }
