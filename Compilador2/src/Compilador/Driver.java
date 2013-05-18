8e9a6e68e6c3990c0214e1532e9e1539c8ef4f01
daniel berrocal <jdanielb1992@gmail.com>
2013-05-18 15:51:26 -0600

    compilador 2.1.2

---
+++ Compilador2/src/Compilador/Driver.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Compilador;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import Compilador.MyException;
+/**
+ *
+ * @author jdbr
+ */
+public class Driver {
+    
+    
+    //public static java_cup.runtime.Scanner s;
+    //Scanner s;
+    public static String E=null;
+    /*public static PrettyPrintAST printer = new PrettyPrintAST() {
+
+        @Override
+        public Object visitTypeconIdAST(TypeconIdAST c, Object arg) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    };*/
+    public static void tokens(File text) throws Exception{
+    try{
+      Scanner s = new Scanner(new FileReader(text));
+     // parser p = new parser(s);
+          //p.parse();
+          
+        System.out.println("IMPRESION DEL ARBOL:");
+        System.out.println("");
+        //printer.imprimir(p.raiz);
+        System.out.println("");
+        System.out.println("FIN DE IMPERSION");
+
+    }
+    catch(Exception e){
+        
+       System.out.println(e.getMessage());
+       E=e.getMessage();
+    } 
+    }
+}
---
+++ Compilador2/src/Compilador/PrettyPrintAST.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Compilador;
+
+/**
+ *
+ * @author jdbr
+ */
+public class PrettyPrintAST {
+    
+}
