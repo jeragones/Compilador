336635d25e6eac391241ad1da92d00302565c309
Jorge Rojas Aragonés <jeragones@gmail.com>
2013-05-20 18:22:54 -0600

    Tablas

---
+++ Compilador2/src/Compilador/Tablas/NodoTabla.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Compilador.Tablas;
+
+import AST.AST;
+
+/**
+ *
+ * @author GeOrge
+ */
+public class NodoTabla {
+    public String nombreID;
+    public int nivel;
+    public AST decl;
+
+    public NodoTabla(String nom, int ni, AST dec){
+        nombreID = nom;
+        nivel = ni;
+        this.decl = dec;
+    }
+}
+
+
---
+++ Compilador2/src/Compilador/Tablas/TablaSimbolos.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Compilador.Tablas;
+
+import AST.AST;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author GeOrge
+ */
+public class TablaSimbolos {
+    private ArrayList tabla;
+    public int nivel;
+    
+    public TablaSimbolos() {
+        tabla = new ArrayList();
+        nivel = 0;
+    }
+    
+    public boolean enter(String n, AST decl){
+        NodoTabla nn = new NodoTabla(n, nivel, decl);  
+        for(int i=tabla.size()-1;i>=0;i++) {
+            if(((NodoTabla)tabla.get(i)).nivel == this.nivel){
+                if(((NodoTabla)tabla.get(i)).nombreID == nn.nombreID)
+                    return false;  
+            }
+        }
+        tabla.add(nn);
+        return true;
+    }
+  
+    public NodoTabla retrieve(String n){
+        for(int i=tabla.size()-1;((i>=0)&&((NodoTabla)tabla.get(i)).nivel >= 0);i++) {
+            NodoTabla temp = (NodoTabla)tabla.get(i);
+            if(temp.nombreID == n)
+            return ((NodoTabla)tabla.get(i));
+        }
+        return null;
+    }
+    
+    public void openScope(){
+        nivel++;
+    }
+  
+    public void closeScope(){
+        //sacar todos los ids de nivel actual de la tabla
+        for(int i=tabla.size()-1;((tabla.size() != 0)&&(((NodoTabla)tabla.get(i)).nivel == this.nivel));i++) {
+            tabla.remove(i);
+        }
+        nivel--;
+    }
+}
