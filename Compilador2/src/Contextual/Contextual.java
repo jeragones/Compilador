e1776aec74c5687c180e1b219d8822ef4733e238
daniel berrocal <jdanielb1992@gmail.com>
2013-05-21 19:14:53 -0600

    acomodado

--- Compilador2/src/Contextual/Contextual.java
+++ Compilador2/src/Contextual/Contextual.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Compilador.Contextual;
+package Contextual;
 
 import AST.*;
 import Contextual.TablaMetodos;
